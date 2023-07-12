7b50a49718cab01bdaf85aded2c681ceb2b9cee1
Marcelo henrique <120139095+marceloteck@users.noreply.github.com>
2023-07-11 21:20:28 -0300

    Update components.js

--- resources/js/components.js
+++ resources/js/components.js
@@ -3,10 +3,8 @@ import { createApp } from 'vue/dist/vue.esm-bundler';
 
 // Importar componentes (menu para o roteamento)
 import menudir from "./components/menudir.vue";
-import navheader from "./components/navheader.vue";
 
 const app = createApp({});
 app.component('menudir', menudir);
-app.component('navheader', navheader);
 
 export default app;
