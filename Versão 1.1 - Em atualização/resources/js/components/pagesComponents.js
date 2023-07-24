7b34e8fa515920306436b4db281fd3651fb0a795
Marcelo henrique <120139095+marceloteck@users.noreply.github.com>
2023-07-24 00:52:18 -0300

    Update pagesComponents.js

--- Versão 1.1 - Em atualização/resources/js/components/pagesComponents.js
+++ Versão 1.1 - Em atualização/resources/js/components/pagesComponents.js
@@ -1,7 +1,7 @@
 // Importar componentes das páginas
 import homeview         from "@/view/contents/homeview.vue";
-import menu1view            from "@/view/contents/menu1.vue";
-import pagina01view          from "@/view/contents/pagina01.vue";
+import menu1view            from "@/view/contents/menu1view.vue";
+import pagina01view          from "@/view/contents/pagina01view.vue";
 
 export{
     menu1view,
